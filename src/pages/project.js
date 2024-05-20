51f1ae276e8cfc6da222ea253a4f2fa9cb2ec53d
edward-w-eth <edwardmeth2@gmail.com>
2024-05-20 01:53:40 -0700

    feat: init project page

---
+++ src/pages/project.js
@@ -0,0 +1,21 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import React from "react";
+
+const project = () => {
+  return (
+    <>
+      <Head>
+        <title>Edward Martin | About Page</title>
+        <meta name="description" content="full-stack developer webGL expert" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <h2 className="text-center font-8xl font-bold">Coming Soon...</h2>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default project;
