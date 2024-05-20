aa58b613396ba126268711c8d596ac0611663c71
edward-w-eth <edwardmeth2@gmail.com>
2024-05-19 23:45:30 -0700

    feat: configure prettier

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,106 +2,106 @@ import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import React, { useEffect, useRef } from "react";
-import Image from 'next/image';
-import aboutMe from '../../public/images/profile/aboutMe.jpg';
+import Image from "next/image";
+import aboutMe from "../../public/images/profile/aboutMe.jpg";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 
-const AnimatedNumnbers = ({value}) => {
-    const ref = useRef(null);
-    const motionValue = useMotionValue(0);
-    const springValue = useSpring(motionValue, { duration: 3000 })
-    const isInView = useInView(ref, {once: true});
+const AnimatedNumnbers = ({ value }) => {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
 
-    useEffect(() => {
-        if(isInView) {
-            motionValue.set(value);
-        }
-    }, [isInView, value, motionValue])
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
 
-    useEffect(() => {
-        springValue.on("change", (latest) => {
-            console.log(latest);
-            if(ref.current && latest.toFixed(0) <= value) {
-                ref.current.textContent = latest.toFixed(0);
-            }
-        })
-    }, [springValue, value]);
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      console.log(latest);
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
 
-    return <span ref={ref}></span>
-}
+  return <span ref={ref}></span>;
+};
 
 const about = () => {
-    return (
-        <>
-            <Head>
-                <title>Edward Martin | About Page</title>
-                <meta name="description" content="full-stack developer webGL expert" />
-            </Head>
-            <main className="flex w-full flex-col items-center justify-center">
-                <Layout className="pt-16">
-                    <AnimatedText text="Stay Hungry, Stay Foolish" className="mb-16" />
-                    <div className="grid w-full grid-cols-8 gap-16">
-                        <div className="col-span-3 flex flex-col items-start justify-start">
-                            <h2 className="mb-4 text-lg font-bold uppercase text-dark">
-                                About Me
-                            </h2>
-                            <p className="font-medium">
-                                {
-                                    `
+  return (
+    <>
+      <Head>
+        <title>Edward Martin | About Page</title>
+        <meta name="description" content="full-stack developer webGL expert" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Stay Hungry, Stay Foolish" className="mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark">
+                About Me
+              </h2>
+              <p className="font-medium">
+                {`
                                     Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
                                     and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
                                     new and innovative ways to bring my clients' visions to life.
-                                    `
-                                }
-
-                            </p>
-                            <p className="font-medium">
-                                {
-                                    `
+                                    `}
+              </p>
+              <p className="font-medium">
+                {`
                                     I believe that design is about more than just making things look pretty.  it's about solving problems and  creating intuitive, enjoyable experiences for users. 
-                                    `
-                                }
-                            
-                            </p>
-                            <p className="font-medicum">
-                                {
-                                    `
+                                    `}
+              </p>
+              <p className="font-medicum">
+                {`
                                     Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
-                                    `
-                                }
-                            
-                            </p>
-                        </div>
-                        <div className="relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark  bg-light p-8 dark:border-light dark:bg-dark">
-                            <div className="absolute  top-0 -right-3 -z-10  rounded-[2rem] rounded-br-3xl  bg-dark dark:bg-light">
-                            </div>
-                            <Image src={aboutMe} alt="About Edward Martin" className="w-full h-auto rounded-2xl" />
-                        </div>
-                        <div className="col-span-2 flex flex-col items-end justify-between">
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold">
-                                    +<AnimatedNumnbers value={20} />
-                                </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">Satisfied Clients</h2>
-                            </div>
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold">
-                                    +<AnimatedNumnbers value={30} />
-                                </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">Project Completed</h2>
-                            </div>
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold">
-                                    +<AnimatedNumnbers value={6} />
-                                </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">Years of Experience</h2>
-                            </div>
-                        </div>
-                    </div>
-                </Layout>
-            </main>
-        </>
-    )
-}
+                                    `}
+              </p>
+            </div>
+            <div className="relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark  bg-light p-8 dark:border-light dark:bg-dark">
+              <div className="absolute  top-0 -right-3 -z-10  rounded-[2rem] rounded-br-3xl  bg-dark dark:bg-light"></div>
+              <Image
+                src={aboutMe}
+                alt="About Edward Martin"
+                className="w-full h-auto rounded-2xl"
+              />
+            </div>
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  +<AnimatedNumnbers value={20} />
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Satisfied Clients
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  +<AnimatedNumnbers value={30} />
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Project Completed
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  +<AnimatedNumnbers value={6} />
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Years of Experience
+                </h2>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
 
-export default about
+export default about;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
-        mont: ['var(--font-mont)', ...fontFamily.sans],
+        mont: ["var(--font-mont)", ...fontFamily.sans],
       },
       colors: {
         dark: "#1b1b1b",
@@ -17,13 +15,13 @@ module.exports = {
         primaryDark: "#58E6D9", // 80,230,217
       },
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
+        "spin-slow": "spin 3s linear infinite",
       },
       backgroundImage: {
-        circularLight: 'repeating-radial-gradient(rgba(0, 0, 0, 0.4) 2px, #f5f5f5 5px, #f5f5f5 100px)'
-      }
+        circularLight:
+          "repeating-radial-gradient(rgba(0, 0, 0, 0.4) 2px, #f5f5f5 5px, #f5f5f5 100px)",
+      },
     },
   },
   plugins: [],
-}
-
+};
