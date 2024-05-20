6641079000825e5ed2b36b3ad4ade00fa68bcf3a
edward-w-eth <edwardmeth2@gmail.com>
2024-05-19 21:59:22 -0700

    feat:landing page

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+            <Layout className="py-8 flex items-center justify-between">
+                <span>2024 &copy; All Right Reserved</span>
+                <div className="flex items-center">
+                    Build with &nbsp;
+                    <Link href="https://github.com/" target={"_blank"}
+                    className="underline underline-offset-2">Edward Martin</Link>
+                </div>
+            </Layout>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,71 @@
 import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+import { DiscordIcon, FigmaIcon, GithubIcon, LinkedinIcon, MailIcon, TelegramIcon } from "./Icon";
 
 const CustomLink = ({href, title, className=""}) => {
+    const router = useRouter();
+    console.log(router.asPath);
     return (
-        <Link href={href}>
+        <Link href={href} className={`${className} relative group`}>
             {title}
+            <span className={`
+            h-[1px] inline-block bg-dark absolute left-0 bottom-0 group-hover:w-full transition-[width] ease duration-300
+            ${router.asPath == href ? 'w-full' : 'w-0'}
+            `}>&nbsp;</span>
         </Link>
     )
 }
+
+const MotionLink = motion(Link);
+
 const NavBar = () => {
     return (
         <header
         className="w-full px-32 py-8 font-medium flex item-center justify-between"
         >
             <nav>
-                <Link href="/">Home</Link>
-                <CustomLink href="/" title="Home"/>
-                <CustomLink href="/about" title="About"/>
-                <CustomLink href="/skill" title="Skills"/>
-                <CustomLink href="/project" title="My Projects"/>
-                <CustomLink href="/contact" title="Contact"/>
+                <CustomLink href="/" title="Home" className="mr-4" />
+                <CustomLink href="/about" title="About" className="mx-4" />
+                <CustomLink href="/skill" title="Skills" className="mx-4"/>
+                <CustomLink href="/project" title="My Projects" className="mx-4"/>
             </nav>
+            <nav className="flex items-center justify-center flex-wrap">
+                <MotionLink href="/" target={"_blank"}
+                whileHover={{y: 2}}
+                whileTap={{scale: 0.9}}
+                >
+                    <MailIcon />
+                </MotionLink>
+                <MotionLink href="/" target={"_blank"}
+                whileHover={{y: 2}}
+                whileTap={{scale: 0.9}}>
+                    <TelegramIcon />
+                </MotionLink>
+                <MotionLink href="/" target={"_blank"}
+                whileHover={{y: 2}}
+                whileTap={{scale: 0.9}}>
+                    <DiscordIcon />
+                </MotionLink>
+                <MotionLink href="/" target={"_blank"}
+                whileHover={{y: 2}}
+                whileTap={{scale: 0.9}}>
+                    <FigmaIcon />
+                </MotionLink>
+                <MotionLink href="/" target={"_blank"}
+                whileHover={{y: 2}}
+                whileTap={{scale: 0.9}}>
+                    <LinkedinIcon />
+                </MotionLink>
+                <MotionLink href="/" target={"_blank"}
+                whileHover={{y: 2}}
+                whileTap={{scale: 0.9}}>
+                    <GithubIcon />
+                </MotionLink>
+            </nav>
+
             <div className="absolute left-[50%] top-2 translate-x-[-50%]">
                 <Logo />
             </div>
